bbdd61fac98c35ed611742ecd7801dd32f627235
Christian052 <doctorshavu@gmail.com>
2025-10-03 11:51:07 +0200

    User inputing

--- userInput.dart
+++ userInput.dart
@@ -1,18 +1,25 @@
 import 'dart:io';
 
 void main() {
+  //To inputing values in default as string
+  print('Enter your name');
+  String? n = stdin.readLineSync();
+
+
   // Ask for first number
   print("Enter the value of x:");
+  // To converte from string to integer
   int? x = int.tryParse(stdin.readLineSync()!);
 
   // Ask for second number
   print("Enter the value of y:");
+  // To converte from string to integer
   int? y = int.tryParse(stdin.readLineSync()!);
 
   // Check if inputs are valid numbers
   if (x != null && y != null) {
     int z = x + y; // calculate sum
-    print("The sum of $x and $y is $z");
+    print("$n The sum of $x and $y is $z");
   } else {
     print("Please enter valid numbers for both x and y.");
   }
